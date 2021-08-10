9f071b134cf25da8766e87c413c32ff0c5763333
shraddha319 <shraddha.jain3219@gmail.com>
2021-08-10 22:47:58 +0530

    feat: add react router v6

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,23 @@
-import './App.css';
-import { Sidebar } from './components';
 import styled from 'styled-components';
 import tw from 'twin.macro';
+import { Routes, Route } from 'react-router-dom';
+import { Sidebar } from './components';
+import { Home } from './pages';
+import './App.css';
 
 const Layout = styled.div`
-  ${tw`w-3/5 h-full mx-auto border-2 grid grid-cols-4`}
+  ${tw`max-w-max h-full mx-auto border-2 grid grid-cols-4`}
 `;
 
 export default function App() {
   return (
     <Layout>
       <Sidebar styles="col-span-1" />
-      <div className="col-span-2 border-2 border-red-500 overflow-y-scroll no-scrollbar"></div>
+      <div className="col-span-2 border-2 border-red-500 overflow-y-scroll no-scrollbar">
+        <Routes>
+          <Route path="/" element={<Home />} />
+        </Routes>
+      </div>
       <div className="col-span-1">Side</div>
     </Layout>
   );
