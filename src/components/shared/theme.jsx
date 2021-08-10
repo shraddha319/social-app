a026cac05a6f8e88dee8c40921d1a62fd76d3acc
shraddha319 <shraddha.jain3219@gmail.com>
2021-08-10 21:59:59 +0530

    style: tw size and color theme config

---
+++ src/components/shared/theme.jsx
@@ -0,0 +1,19 @@
+import tw from 'twin.macro';
+
+export const twSize = {
+  tiny: tw`text-xs`,
+  small: tw`text-sm`,
+  base: tw`text-base`,
+  large: tw`text-lg`,
+  xl: tw`text-xl`,
+  xl2: tw`text-2xl`,
+  xl3: tw`text-3xl`,
+  xl4: tw`text-4xl`,
+};
+
+export const twColor = {
+  primary: tw`text-blue-500`,
+  secondary: tw`text-red-500`,
+  action: tw`text-gray-500`,
+  disabled: tw`text-gray-500`,
+};
