c0cd03fa11103b74d2bd150b1339ac24f5dcc828
shraddha319 <shraddha.jain3219@gmail.com>
2021-08-10 22:01:59 +0530

    feat: 3 col layout of app

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,18 @@
 import './App.css';
+import { Sidebar } from './components';
+import styled from 'styled-components';
+import tw from 'twin.macro';
 
-function App() {
-  return <div className="App"></div>;
+const Layout = styled.div`
+  ${tw`w-3/5 h-full mx-auto border-2 grid grid-cols-4`}
+`;
+
+export default function App() {
+  return (
+    <Layout>
+      <Sidebar styles="col-span-1" />
+      <div className="col-span-2 border-2 border-red-500 overflow-y-scroll no-scrollbar"></div>
+      <div className="col-span-1">Side</div>
+    </Layout>
+  );
 }
-
-export default App;
