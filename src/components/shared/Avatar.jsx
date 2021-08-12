e25cecb0d0410ec0af452f1a1991e1bd712b00b1
shraddha319 <shraddha.jain3219@gmail.com>
2021-08-12 22:26:54 +0530

    style: add sizes, white border to avatar

--- src/components/shared/Avatar.jsx
+++ src/components/shared/Avatar.jsx
@@ -7,6 +7,9 @@ const twAvatar = {
     small: tw`h-10 w-10 text-xl`,
     medium: tw`h-12 w-12 text-2xl`,
     large: tw`h-14 w-14 text-3xl`,
+    xl: tw`h-16 w-16 text-4xl`,
+    xl2: tw`h-20 w-20 text-6xl`,
+    xl3: tw`h-24 w-24 text-8xl`,
   },
 };
 
@@ -19,16 +22,18 @@ const bg = [
   tw`bg-pink-500`,
 ];
 
-export const Avatar = styled.img(({ size }) => [
-  tw`rounded-full object-contain`,
+export const Avatar = styled.img(({ size, border }) => [
+  tw`rounded-full object-cover`,
   twAvatar.size[size],
+  border && tw`border-4 border-white`,
 ]);
 
-export const FallbackAvatar = styled.div(({ size }) => {
+export const FallbackAvatar = styled.div(({ size, border }) => {
   const index = Math.trunc(Math.random() * bg.length - 1);
   return [
     tw`rounded-full flex justify-center items-center text-white cursor-default uppercase`,
     twAvatar.size[size],
     bg[index],
+    border && tw`border-4 border-white`,
   ];
 });
