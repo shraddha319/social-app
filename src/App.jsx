b933129e6ab7902f648ec40a9ef4e0194a4f5ac4
shraddha319 <shraddha.jain3219@gmail.com>
2021-08-12 22:20:49 +0530

    feat: add topbar for app

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,42 @@
 import styled from 'styled-components';
 import tw from 'twin.macro';
-import { Routes, Route } from 'react-router-dom';
+import { Routes, Route, useLocation } from 'react-router-dom';
 import { Sidebar } from './components';
-import { Home } from './pages';
+import { IconButton } from './components';
+import { Home, User } from './pages';
+import { ArrowBack } from './assets/icons';
 import './App.css';
 
 const Layout = styled.div`
-  ${tw`w-3/5 h-full mx-auto grid grid-cols-4`}
+  ${tw`h-full mx-auto grid grid-cols-4`}
+  width: 1200px;
+`;
+
+const TopBar = styled.div`
+  ${tw`w-full sticky top-0 h-12 p-4 text-lg bg-white shadow flex items-center space-x-4 font-medium z-10`}
 `;
 
 export default function App() {
+  const { pathname } = useLocation();
   return (
     <Layout>
       <Sidebar styles="col-span-1" />
       <div className="col-span-2 overflow-y-scroll no-scrollbar">
+        <TopBar>
+          {pathname !== '/' && (
+            <>
+              <IconButton variant="primary" size="small">
+                <ArrowBack />
+              </IconButton>
+              <p>
+                {pathname.slice(1).charAt(0).toUpperCase() + pathname.slice(2)}
+              </p>
+            </>
+          )}
+        </TopBar>
         <Routes>
           <Route path="/" element={<Home />} />
+          <Route path="/profile/" element={<User />} />
         </Routes>
       </div>
       <div className="col-span-1">Side</div>
