0da5cf2c7faa3c740444115f1a6c5765854f3ce5
shraddha319 <shraddha.jain3219@gmail.com>
2021-09-14 22:50:26 +0530

    refactor: form validation constants

---
+++ src/constants/index.js
@@ -0,0 +1,13 @@
+export const formError = {
+  EMAIL_UNIQUE: 'Email already exists.',
+  EMAIL_INVALID: 'Please enter valid email.',
+  USERNAME_UNIQUE: 'Username already exists.',
+  USERNAME_INVALID: 'Username can be alphanumeric only.',
+  PASSWORD_LEN: 'Password must be atleast 8 characters long.',
+  PASSWORD_INVALID:
+    'Password must contain at least one letter, one number and one special character.',
+  PASSWORD_MATCH: 'Passwords do not match.',
+  FIELD_REQUIRED: 'This is a required field',
+  USERNAME_EMAIL_INVALID: 'Please enter valid email or password',
+  LETTERS_ONLY: 'Alphabets only.',
+};
