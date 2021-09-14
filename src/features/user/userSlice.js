7203810eea1929a89f0d1aa3f7e4c1450e14c303
shraddha319 <shraddha.jain3219@gmail.com>
2021-09-15 02:37:11 +0530

    feat: update profile async action

--- src/features/user/userSlice.js
+++ src/features/user/userSlice.js
@@ -1,4 +1,26 @@
-import { createSlice } from '@reduxjs/toolkit';
+import { createSlice, createAsyncThunk } from '@reduxjs/toolkit';
+import { updateUser } from './userAPI';
+
+export const updateProfile = createAsyncThunk(
+  'user/updateProfile',
+  async (update, { getState, rejectWithValue }) => {
+    try {
+      const {
+        user: {
+          user: { _id },
+        },
+        auth: { token },
+      } = getState();
+      await updateUser(_id, token, update);
+      return update;
+    } catch (err) {
+      if (!err.response) {
+        throw err;
+      }
+      return rejectWithValue(err.response.data);
+    }
+  }
+);
 
 const userSlice = createSlice({
   name: 'user',
@@ -6,12 +28,31 @@ const userSlice = createSlice({
   initialState: {
     status: 'idle',
     user: null,
+    error: null,
   },
 
   reducers: {
     setUser: (state, action) => {
-      state.status = 'success';
       state.user = action.payload.user;
+    },
+  },
+
+  extraReducers: {
+    [updateProfile.pending]: (state) => {
+      state.status = 'loading';
+    },
+    [updateProfile.fulfilled]: (state, action) => {
+      Object.keys(action.payload).forEach((field) => {
+        console.log(field, state.user[field], action.payload[field]);
+        state.user[field] = action.payload[field];
+      });
+      state.status = 'success';
+    },
+    [updateProfile.rejected]: (state, action) => {
+      state.status = 'failed';
+      state.error = action.payload
+        ? action.payload.error
+        : action.error.message;
     },
   },
 });
