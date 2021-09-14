a90e902bcd06a9fe1479501a6addec1a76fdd5cb
shraddha319 <shraddha.jain3219@gmail.com>
2021-09-15 05:02:27 +0530

    configure posts reducer in store

--- src/app/store.js
+++ src/app/store.js
@@ -2,11 +2,13 @@ import { configureStore } from '@reduxjs/toolkit';
 import counterReducer from '../features/counter/counterSlice';
 import authReducer from '../features/auth/authSlice';
 import userReducer from '../features/user/userSlice';
+import postsReducer from '../features/posts/postsSlice';
 
 export const store = configureStore({
   reducer: {
     counter: counterReducer,
     auth: authReducer,
     user: userReducer,
+    posts: postsReducer,
   },
 });
