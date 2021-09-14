f396997d83356ef3ba8ea1a84018f96c42f06a57
shraddha319 <shraddha.jain3219@gmail.com>
2021-09-14 22:49:58 +0530

    feat: add login and signup page

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,23 +1,87 @@
+import { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+import { useSelector, useDispatch } from 'react-redux';
 import { Button, Typography, StyledLink, Input } from '../components';
+import { loginValidationRules, validate } from '../validations';
+import { loginUser } from '../features/auth/authSlice';
+import { useEffect } from 'react';
 
 export default function Login() {
+  const [input, setInput] = useState({
+    email: '',
+    password: '',
+  });
+  const [inputError, setInputError] = useState({
+    login: '',
+    email: '',
+    password: '',
+  });
+  const dispatch = useDispatch();
+  const { status, error, token } = useSelector((state) => state.auth);
+  const navigate = useNavigate();
+
+  function loginHandler() {
+    const errors = validate(input, loginValidationRules);
+    setInputError(errors);
+    if (Object.keys(errors).length === 0) {
+      if (status === 'idle' || status === 'failed') dispatch(loginUser(input));
+    }
+  }
+
+  useEffect(() => {
+    if (status === 'failed' && error.statusCode === 401)
+      setInputError({
+        email: '',
+        password: '',
+        login: 'Invalid username/password',
+      });
+
+    if (status === 'success') {
+      navigate('/feed');
+    }
+  }, [status, navigate, error]);
+
   return (
     <div className="h-full w-full flex items-center justify-center">
-      <form className="w-80 space-y-8 p-6 border-gray-300 border-2 rounded-lg">
+      <form
+        className="w-80 space-y-8 p-6 border-gray-300 border-2 rounded-lg"
+        onSubmit={(e) => e.preventDefault()}
+      >
         <Typography align="center" color="textPrimary" variant="h6" as="h1">
           Login
         </Typography>
+        <Typography color="error" variant="small" className="p-1">
+          {inputError.login}
+        </Typography>
         <Input>
-          <input type="email" id="email" placeholder=" " />
+          <input
+            type="email"
+            id="email"
+            placeholder=" "
+            value={input.email}
+            onChange={(e) => setInput({ ...input, email: e.target.value })}
+          />
           <label htmlFor="email" name="email">
             Email
           </label>
+          <Typography color="error" variant="small" className="p-1">
+            {inputError.email}
+          </Typography>
         </Input>
         <Input>
-          <input type="password" id="password" placeholder=" " />
+          <input
+            type="password"
+            id="password"
+            placeholder=" "
+            value={input.password}
+            onChange={(e) => setInput({ ...input, password: e.target.value })}
+          />
           <label htmlFor="password" name="password">
             Password
           </label>
+          <Typography color="error" variant="small" className="p-1">
+            {inputError.password}
+          </Typography>
         </Input>
         <Button
           type="submit"
@@ -25,8 +89,9 @@ export default function Login() {
           rounded
           variant="solidPrimary"
           size="large"
+          onClick={loginHandler}
         >
-          Log in
+          {status === 'loading' ? 'Loading...' : 'Log In'}
         </Button>
         <p className="space-x-2 flex justify-center text-blue-500">
           <StyledLink to="/">Forgot password?</StyledLink>
--- src/pages/Signup.jsx
+++ src/pages/Signup.jsx
@@ -1,52 +1,167 @@
+import { useState, useEffect } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+import { useNavigate } from 'react-router-dom';
 import { Typography, Input, Button } from '../components';
+import { signupValidationRules, validate } from '../validations';
+import { registerUser } from '../features/auth/authSlice';
 
 export default function Signup() {
+  const [input, setInput] = useState({
+    name: '',
+    username: '',
+    email: '',
+    password: '',
+    confirmPassword: '',
+    dob: '',
+  });
+  const [inputError, setInputError] = useState({
+    name: '',
+    username: '',
+    email: '',
+    password: '',
+    confirmPassword: '',
+    dob: '',
+    signup: '',
+  });
+  const dispatch = useDispatch();
+  const { status, error, token } = useSelector((state) => state.auth);
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    if (status === 'failed' && error.statusCode === 400)
+      setInputError(
+        error.errors.reduce((errObj, { message, key, type }) => {
+          return { ...errObj, [key]: message };
+        }, {})
+      );
+    if (status === 'success' && token) {
+      navigate('/feed');
+    }
+  }, [status, error, token, navigate]);
+
+  function signupHandler() {
+    const inputErrors = validate(input, signupValidationRules);
+    setInputError(inputErrors);
+    if (Object.keys(inputErrors).length === 0) {
+      const user = (({ name, username, email, password, dob }) => ({
+        name,
+        username,
+        email,
+        password,
+        dob,
+      }))(input);
+
+      if (status === 'idle' || status === 'failed')
+        dispatch(registerUser(user));
+    }
+  }
+
   return (
     <div className="h-full w-full flex items-center justify-center">
-      <form className="w-96 space-y-6 py-6 px-6 border-gray-300 border-2 rounded-lg">
+      <form
+        className="w-96 space-y-6 py-6 px-6 border-gray-300 border-2 rounded-lg"
+        onSubmit={(e) => e.preventDefault()}
+      >
         <Typography align="left" color="textPrimary" variant="h6" as="h1">
           Create your account
         </Typography>
         <p className="flex space-x-1 w-full">
           <Input>
-            <input type="text" id="name" placeholder=" " />
+            <input
+              value={input.name}
+              type="text"
+              id="name"
+              placeholder=" "
+              onChange={(e) => setInput({ ...input, name: e.target.value })}
+            />
             <label htmlFor="name" name="name">
-              Full name
+              Name
             </label>
+            <Typography color="error" variant="small" className="p-1">
+              {inputError.name}
+            </Typography>
           </Input>
           <Input>
-            <input type="text" id="username" placeholder=" " />
+            <input
+              value={input.username}
+              type="text"
+              id="username"
+              placeholder=" "
+              onChange={(e) => setInput({ ...input, username: e.target.value })}
+            />
             <label htmlFor="username" name="username">
               Username
             </label>
+            <Typography color="error" variant="small" className="p-1">
+              {inputError.username}
+            </Typography>
           </Input>
         </p>
         <Input>
-          <input type="email" id="email" placeholder=" " />
+          <input
+            value={input.email}
+            type="email"
+            id="email"
+            placeholder=" "
+            onChange={(e) => setInput({ ...input, email: e.target.value })}
+          />
           <label htmlFor="email" name="email">
             Email
           </label>
+          <Typography color="error" variant="small" className="p-1">
+            {inputError.email}
+          </Typography>
         </Input>
         <Input>
-          <input type="password" id="password" placeholder=" " />
+          <input
+            value={input.password}
+            type="password"
+            id="password"
+            placeholder=" "
+            onChange={(e) => setInput({ ...input, password: e.target.value })}
+          />
           <label htmlFor="password" name="password">
             Password
           </label>
+          <Typography color="error" variant="small" className="p-1">
+            {inputError.password}
+          </Typography>
         </Input>
         <Input>
-          <input type="password" id="confirm_password" placeholder=" " />
+          <input
+            value={input.confirmPassword}
+            type="password"
+            id="confirm_password"
+            placeholder=" "
+            onChange={(e) =>
+              setInput({ ...input, confirmPassword: e.target.value })
+            }
+          />
           <label htmlFor="confirm_password" name="confirm_password">
             Confirm Password
           </label>
+          <Typography color="error" variant="small" className="p-1">
+            {inputError.confirmPassword}
+          </Typography>
         </Input>
         <Input>
-          <input type="date" id="dob" placeholder=" " />
+          <input
+            value={input.dob}
+            type="date"
+            id="dob"
+            placeholder=" "
+            onChange={(e) => setInput({ ...input, dob: e.target.value })}
+          />
           <label htmlFor="dob" name="dob">
             Date of Birth
           </label>
-          <Typography variant="small" className="p-1">
-            Confirm your own age, even if this account is for a business, a pet,
-            or something else.
+          <Typography
+            color={inputError.dob ? 'inputError' : ''}
+            variant="small"
+            className="p-1"
+          >
+            {inputError.dob ||
+              'Confirm your own age, even if this account is for a business, a pet, or something else.'}
           </Typography>
         </Input>
         <Button
@@ -55,8 +170,9 @@ export default function Signup() {
           rounded
           variant="solidPrimary"
           size="large"
+          onClick={signupHandler}
         >
-          Sign up
+          {status === 'loading' ? 'Loading...' : 'Sign up'}
         </Button>
       </form>
     </div>
