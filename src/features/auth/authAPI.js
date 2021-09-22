395dc6dfa40100a9c1f80fb1d243aff3df5f1372
shraddha319 <shraddha.jain3219@gmail.com>
2021-09-22 08:43:51 +0530

    fix: include login by auth token

--- src/features/auth/authAPI.js
+++ src/features/auth/authAPI.js
@@ -1,9 +1,31 @@
-import axios from 'axios';
+import API from '../api.config';
 
+/**
+ *
+ * @param {Object: {
+ * email: String,
+ * password: String,
+ * name: String,
+ * username: String,
+ * dob: String}} user
+ */
 export const register = (user) => {
-  return axios.post('http://localhost:3001/users', user);
+  return API.post('/users', user);
 };
 
+/**
+ *
+ * @param {email: String, password: String, authToken: JWT} credentials
+ */
 export const login = (credentials) => {
-  return axios.post('http://localhost:3001/auth/login', credentials);
+  if (credentials.email && credentials.password)
+    return API.post('/auth/login', credentials);
+  else
+    return API.post(
+      '/auth/login',
+      {},
+      {
+        headers: { Authorization: credentials.authToken },
+      }
+    );
 };
